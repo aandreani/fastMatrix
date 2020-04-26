6f1f741e830ac3cf3c93c98362ae1c4803f7894d
ndriabecks <32883013+aandreani@users.noreply.github.com>
2020-04-26 19:07:26 +0200

    Update fastMatrix.c
    minor corrections

--- fastMatrix.c
+++ fastMatrix.c
@@ -6,7 +6,7 @@ void sum_4_int(int* A, int* B, int* C) {
 	a = _mm_loadu_si128((const __m128i*)(A));
 	b = _mm_loadu_si128((const __m128i*)(B));
 	c = _mm_add_epi32(a,b);
-	__mm_storeu_si128((__m128i*)C, c);
+	_mm_storeu_si128((__m128i*)C, c);
 }
 
 void sum_8_int(int* A, int* B, int* C) {
@@ -22,7 +22,7 @@ void sum_int_matrix(int** A, int** B, int** C, int r, int c) {
         int j = 0;
         for(j; j+7 < c; j += 8) sum_8_int(A[i]+j, B[i]+j, C[i]+j);
         for(j; j+3 < c; j += 4) sum_4_int(A[i]+j, B[i]+j, C[i]+j);
-        for(j; j < c; j++) C[i] = A[i][j] + B[i][j];
+        for(j; j < c; j++) C[i][j] = A[i][j] + B[i][j];
     }
 
 }

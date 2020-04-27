0a8a1dedd50f8631b60e08847aae65ed268099e9
ndriabecks <32883013+aandreani@users.noreply.github.com>
2020-04-27 06:32:57 +0200

    Update main.c
    Added profiling for cheking the performance boost

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <time.h> // performance analysis
+
 typedef struct matrix
 {
     int ** m;
@@ -45,6 +47,14 @@ void delete_matrix(matrix* mat) {
     free(mat);
 }
 
+void matrix_sum(matrix* A, matrix* B, matrix* C) {
+    int r = A->r, c = A->c;
+    int i = 0, j;
+    int** a = A->m, ** b = B->m, ** cc = C->m;
+    for(i; i < r; i++) 
+        for(j = 0; j < c; j++) cc[i][j] = a[i][j] + b[i][j];
+}
+
 int main() {
     printf("Welcome to fastMatrix library test!\n");
     printf("[+] Allocating some matrices...");
@@ -52,8 +62,8 @@ int main() {
     matrix* B = new_matrix(2, 2);
     matrix* C = new_matrix(5, 5);
     matrix* D = new_matrix(5, 5);
-    matrix* E = new_matrix(4, 44);
-    matrix* F = new_matrix(4, 44);
+    matrix* E = new_matrix(512, 1024);
+    matrix* F = new_matrix(512, 1024);
     init_matrix(A); init_matrix(B);
     init_matrix(C); init_matrix(D);
     init_matrix(E); init_matrix(F);
@@ -64,21 +74,26 @@ int main() {
     printf("B:\n");
     print_matrix(B);
 
-    printf("*** Matrix sum test section ***\n");
-    printf("Computing some sums...");
-    matrix* ab = new_matrix(A->r, A->c);
-    sum_int_matrix(A->m, B->m, ab->m, A->r, A->c);
+    clock_t t;
+    double tt;
 
-    matrix* cd = new_matrix(C->r, C->c);
-    sum_int_matrix(C->m, D->m, cd->m, C->r, C->c);
+    printf("*** Matrix sum test section ***\n");
 
     matrix* ef = new_matrix(E->r, E->c);
-    sum_int_matrix(E->m, F->m, ef->m, E->r, E->c);
-    printf("done!\n");
+    matrix* sef = new_matrix(E->r, E->c);
+    printf("Computing times for E + F (512x1024):\n");
 
-    printf("A + B:\n"); print_matrix(ab);
-    printf("C + D:\n"); print_matrix(cd);
-    printf("E + F:\n"); print_matrix(ef);
+    t = clock();
+    matrix_sum(E, F, sef);
+    t = clock()-t;
+    tt = ((double)t)/CLOCKS_PER_SEC;
+    printf("slow: %f\t", tt);
+
+    t = clock();
+    sum_int_matrix(E->m, F->m, ef->m, E->r, E->c);
+    t = clock()-t;
+    tt = ((double)t)/CLOCKS_PER_SEC;
+    printf("fast: %f\t\n", tt);
 
 
     printf("[+] Cleaning memory...");
@@ -88,5 +103,7 @@ int main() {
     delete_matrix(D);
     delete_matrix(E);
     delete_matrix(F);
+    delete_matrix(ef);
+    delete_matrix(sef);
     printf("done!\n");
 }

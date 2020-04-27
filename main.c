edb3bbb0d351ceb3b2a1642db25ccfb1d3195644
ndriabecks <32883013+aandreani@users.noreply.github.com>
2020-04-27 09:27:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,6 @@
 #include "matrix.h"
 #include "fastMatrix.h"
 
-
-
-
-
 int main() {
     printf("Welcome to fastMatrix library test!\n");
 
@@ -28,6 +24,7 @@ int main() {
     printf("*** Matrix mul test section ***\n");
 
     matrix* ab = new_matrix(A->r, B->c);
+    matrix* fab = new_matrix(A->r, B->c);
     printf("[+] Computing times for E x F (%dx%d, %dx%d):\n", 
         A->r, A->c, B->r, B->c);
 
@@ -38,7 +35,7 @@ int main() {
     printf("slow: %f\t", tt);
 
     t = clock();
-    mul_t(A->m, B->m, ab->m, A->r, B->c, B->r);
+    mul_t(A->m, B->m, fab->m, A->r, B->c, B->r);
     t = clock()-t;
     tt = ((double)t)/CLOCKS_PER_SEC;
     printf("fast: %f\t\n", tt);
@@ -47,5 +44,7 @@ int main() {
     printf("[+] Cleaning memory...");
     delete_matrix(A);
     delete_matrix(B);
+    delete_matrix(ab);
+    delete_matrix(fab);
     printf("done!\n");
 }
